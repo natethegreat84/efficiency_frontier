5a84fe93b99e9941807b7dcb3958da14244bc4c5
natethegreat84 <144256165+natethegreat84@users.noreply.github.com>
2024-09-20 14:02:42 -0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -7,6 +7,7 @@ import plotly.express as px
 import pandas as pd   
 import plotly.graph_objects as go
 import numpy as np
+ 
 from datetime import date, datetime, timedelta
  
 import yfinance as yf
@@ -90,188 +91,189 @@ def simulation_engine(close_price_df ,weights, initial_investment, risk_free_rat
  
 one_year_treasury_list = fred.get_series('DGS1')
 oy_df = one_year_treasury_list.to_frame(name='Yield')
-
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SPACELAB])
+ 
+app = Dash(__name__, external_stylesheets=[dbc.themes.SPACELAB])
 server = app.server
  
 app.layout = html.Div(
-    id = "app-container",
-    children=[html.H1("Efficency Frontier POC"),
-        html.P("This is a POC analysis"),
-        html.Br(),
-        html.H2("Enter the number of simulations you want to run."),
-        dcc.Input(value =500, type = 'number', id='monte-carlo-sims', persistence=True, persistence_type='session'),
-        html.Br(),
-        html.Br(),
-        html.H2("Select stocks to be in the portfolio from the dropdown"),
-        dcc.Dropdown(['AAPL', 'AMZN',   'CAT',  'DE', 'DUK', 'EXC', 'GOOGL', 'JNJ', 'JPM', 'META', 'PFE', 'PG', 'WEC', 'F', 'GM', 'COKE', 'Add New'],
-                     ['AAPL', 'F', 'JPM']
-                     , multi=True, persistence=True, persistence_type='session'
-                     , id = 'stock-ticker-dropdown'),
-        html.Br(),
-        html.Br(),
-        html.H2("Select a starting date.  The simulation will use data for one year."),
-        dcc.DatePickerSingle(
-             id='my-date-picker-range',
-             min_date_allowed=date(2016,1, 1),
-             max_date_allowed=max_date,
-             date=date(2023, 5, 1),
-             clearable=True
-             ),
-        html.P("The earliest date available is 1 year ago today"),
-        html.Br(),        
-        html.P(id='treasury-rate-display'),        
-        html.Br(),
-        dcc.Loading(dcc.Graph(id='stock-price-returns'), type='circle'),
-        html.Br(),
-        dcc.Loading(dcc.Graph(id='portfolio-line-graph'), type = 'graph'),
-        html.Div(id='optimal-portfolio-stats'),
-        html.Div(id='best-weights-table')
-    ]
+    id = "app-container", 
+    children=[html.H1("Efficency Frontier POC"),
+        html.P("This is a POC analysis"),
+        html.Br(),
+        html.H2("Enter the number of simulations you want to run."),
+        dcc.Input(value =500, type = 'number', id='monte-carlo-sims', persistence=True, persistence_type='session'),
+        html.Br(),
+        html.Br(),
+        html.H2("Select stocks to be in the portfolio from the dropdown"),
+        dcc.Dropdown(['AAPL', 'AMZN',  'CAT',  'DE', 'DUK', 'EXC', 'GOOGL', 'JNJ', 'JPM', 'META', 'PFE', 'PG', 'WEC', 'F', 'GM', 'COKE', 'Add New'],
+                     ['AAPL', 'F', 'JPM']
+                     , multi=True, persistence=True, persistence_type='session'
+                     , id = 'stock-ticker-dropdown'),
+        html.Br(),
+        html.Br(),
+        html.H2("Select a starting date.  The simulation will use data for one year."),
+        dcc.DatePickerSingle(
+             id='my-date-picker-range',
+             min_date_allowed=date(2016,1, 1),
+             max_date_allowed=max_date,
+             date=date(2023, 5, 1),
+             clearable=True
+             ),
+        html.P("The earliest date available is 1 year ago today"),
+        html.Br(),        
+        html.P(id='treasury-rate-display'),        
+        html.Br(),
+        dcc.Loading(dcc.Graph(id='stock-price-returns'), type='circle'),
+        html.Br(),
+        dcc.Loading(dcc.Graph(id='portfolio-line-graph'), type = 'graph'),
+        html.Div(id='optimal-portfolio-stats'),
+        html.Div(id='best-weights-table')
+    ]
 )
  
+
 @callback(
-        Output('stock-price-returns', 'figure'),
-        Output('portfolio-line-graph', 'figure'),
-        Output('optimal-portfolio-stats', 'children'),
-        Output('treasury-rate-display', 'children'),
-        Output('best-weights-table', 'children'),
-        Input('my-date-picker-range', 'date'),
-        Input('stock-ticker-dropdown', 'value'),
-        Input('monte-carlo-sims', 'value'),
-              )
+        Output('stock-price-returns', 'figure'),
+        Output('portfolio-line-graph', 'figure'),
+        Output('optimal-portfolio-stats', 'children'),
+        Output('treasury-rate-display', 'children'),
+        Output('best-weights-table', 'children'),
+        Input('my-date-picker-range', 'date'),
+        Input('stock-ticker-dropdown', 'value'),
+        Input('monte-carlo-sims', 'value'),
+              )
 def display_stock_output(start_date, tickers, sims):
  
-    end_date = datetime.strptime(start_date, '%Y-%m-%d') + timedelta(days=365)
+    end_date = datetime.strptime(start_date, '%Y-%m-%d') + timedelta(days=365)
  
-    end_date = datetime.strftime(end_date, '%Y-%m-%d')
+    end_date = datetime.strftime(end_date, '%Y-%m-%d')
  
-    end_date = datetime.strptime(start_date, '%Y-%m-%d') + timedelta(days=365)
+    end_date = datetime.strptime(start_date, '%Y-%m-%d') + timedelta(days=365)
  
-    end_date = datetime.strftime(end_date, '%Y-%m-%d')
-   
-    data = yf.download(tickers, start = start_date, end = end_date)
+    end_date = datetime.strftime(end_date, '%Y-%m-%d')
+    
+    data = yf.download(tickers, start = start_date, end = end_date)
  
-    closing_prices = data["Adj Close"]
-    closing_prices.reset_index(inplace=True)
-   
-    new_closing_prices = closing_prices
+    closing_prices = data["Adj Close"]
+    closing_prices.reset_index(inplace=True)
+    
+    new_closing_prices = closing_prices
  
-    n = len(new_closing_prices.columns)-1
-   
-    rfr = oy_df.loc[start_date]['Yield']
+    n = len(new_closing_prices.columns)-1
+    
+    rfr = oy_df.loc[start_date]['Yield']
  
-    sim_runs = sims
-    initial_investment = 100000000
+    sim_runs = sims
+    initial_investment = 100000000
  
-    # Placeholder to store all weights
-    weights_runs = np.zeros((sim_runs, n))
+    # Placeholder to store all weights
+    weights_runs = np.zeros((sim_runs, n))
  
-    # Placeholder to store all Sharpe ratios
-    sharpe_ratio_runs = np.zeros(sim_runs)
+    # Placeholder to store all Sharpe ratios
+    sharpe_ratio_runs = np.zeros(sim_runs)
  
-    # Placeholder to store all expected returns
-    expected_portfolio_returns_runs = np.zeros(sim_runs)
+    # Placeholder to store all expected returns
+    expected_portfolio_returns_runs = np.zeros(sim_runs)
  
-    # Placeholder to store all volatility values
-    volatility_runs = np.zeros(sim_runs)
+    # Placeholder to store all volatility values
+    volatility_runs = np.zeros(sim_runs)
  
-    # Placeholder to store all returns on investment
-    return_on_investment_runs = np.zeros(sim_runs)
+    # Placeholder to store all returns on investment
+    return_on_investment_runs = np.zeros(sim_runs)
  
-    # Placeholder to store all final portfolio values
-    final_value_runs = np.zeros(sim_runs)
+    # Placeholder to store all final portfolio values
+    final_value_runs = np.zeros(sim_runs)
  
-    # Placeholder to store all final duration values
-    # final_duration_runs = np.zeros(sim_runs)
- 
- 
-    for i in range(sim_runs):
-        # Generate random weights
-        weights = generate_portfolio_weights(n)
-        # Store the weights.
-        weights_runs[i,:] = weights
-       
-        # Call "simulation_engine" function and store Sharpe ratio, return and volatility
-        # Note that asset allocation is performed using the "asset_allocation" function  
-        expected_portfolio_returns_runs[i], volatility_runs[i], sharpe_ratio_runs[i], final_value_runs[i], return_on_investment_runs[i] = simulation_engine(new_closing_prices,weights, initial_investment, rfr)
- 
-    best_weights = weights_runs[sharpe_ratio_runs.argmax(), :]
- 
-    optimal_portfolio_return, optimal_volatility, optimal_sharpe_ratio, highest_final_value, optimal_return_on_investment = simulation_engine(new_closing_prices,weights_runs[sharpe_ratio_runs.argmax(), :], initial_investment, rfr)
- 
-    optimal_portfolio_df = asset_allocation(new_closing_prices, best_weights, initial_investment)
-    optimal_portfolio_df.round(2)
- 
-    sim_out_df = pd.DataFrame({'Volatility': volatility_runs.tolist(), 'Portfolio_Return': expected_portfolio_returns_runs.tolist(), 'Sharpe_Ratio': sharpe_ratio_runs.tolist() })
-    sim_out_df['size'] = (sim_out_df['Sharpe_Ratio'] + 10)
- 
-    x_free = float(0)
-    y_free = float(rfr/100)
- 
-    fig = px.scatter(sim_out_df, x = 'Volatility', y = 'Portfolio_Return', color = 'Sharpe_Ratio', size = 'size', hover_data = ['Sharpe_Ratio'] )
-    fig.add_trace(go.Scatter(x = [x_free], y = [y_free], mode="markers", name = "Risk Free Rate", marker = dict(size=[15], color = 'green')))
-    fig.update_layout(coloraxis_colorbar = dict(y = 0.7, dtick = 5))
-    fig.update_layout({'plot_bgcolor': "white"})
+    # Placeholder to store all final duration values
+    # final_duration_runs = np.zeros(sim_runs)
  
  
-   
-    fig2= px.line(title = 'Total Portfolio Value [$]')
-   
-    # For loop that plots all stock prices in the pandas dataframe df
-    # Note that index starts with 1 because we want to skip the date column
-   
-    for i in optimal_portfolio_df[['Date', 'Portfolio Value [$]']].columns[1:]:
-        fig2.add_scatter(x = optimal_portfolio_df['Date'], y = optimal_portfolio_df[i], name = i)
-        fig2.update_traces(line_width = 2)
-        fig2.update_layout({'plot_bgcolor': "white"})
+    for i in range(sim_runs):
+        # Generate random weights 
+        weights = generate_portfolio_weights(n)
+        # Store the weights.
+        weights_runs[i,:] = weights
+        
+        # Call "simulation_engine" function and store Sharpe ratio, return and volatility
+        # Note that asset allocation is performed using the "asset_allocation" function  
+        expected_portfolio_returns_runs[i], volatility_runs[i], sharpe_ratio_runs[i], final_value_runs[i], return_on_investment_runs[i] = simulation_engine(new_closing_prices,weights, initial_investment, rfr)
+ 
+    best_weights = weights_runs[sharpe_ratio_runs.argmax(), :]
+ 
+    optimal_portfolio_return, optimal_volatility, optimal_sharpe_ratio, highest_final_value, optimal_return_on_investment = simulation_engine(new_closing_prices,weights_runs[sharpe_ratio_runs.argmax(), :], initial_investment, rfr)
+ 
+    optimal_portfolio_df = asset_allocation(new_closing_prices, best_weights, initial_investment)
+    optimal_portfolio_df.round(2)
+ 
+    sim_out_df = pd.DataFrame({'Volatility': volatility_runs.tolist(), 'Portfolio_Return': expected_portfolio_returns_runs.tolist(), 'Sharpe_Ratio': sharpe_ratio_runs.tolist() })
+    sim_out_df['size'] = (sim_out_df['Sharpe_Ratio'] + 10)
+ 
+    x_free = float(0)
+    y_free = float(rfr/100)
+ 
+    fig = px.scatter(sim_out_df, x = 'Volatility', y = 'Portfolio_Return', color = 'Sharpe_Ratio', size = 'size', hover_data = ['Sharpe_Ratio'] )
+    fig.add_trace(go.Scatter(x = [x_free], y = [y_free], mode="markers", name = "Risk Free Rate", marker = dict(size=[15], color = 'green')))
+    fig.update_layout(coloraxis_colorbar = dict(y = 0.7, dtick = 5))
+    fig.update_layout({'plot_bgcolor': "white"})
  
  
-    risk_free_rate_text = ('''The 1 year treasury yield at the start is {:,.02}%.  Start date is {} and end date is {}.
+    
+    fig2= px.line(title = 'Total Portfolio Value [$]')
+    
+    # For loop that plots all stock prices in the pandas dataframe df
+    # Note that index starts with 1 because we want to skip the date column
+    
+    for i in optimal_portfolio_df[['Date', 'Portfolio Value [$]']].columns[1:]:
+        fig2.add_scatter(x = optimal_portfolio_df['Date'], y = optimal_portfolio_df[i], name = i)
+        fig2.update_traces(line_width = 2)
+        fig2.update_layout({'plot_bgcolor': "white"})
+ 
+ 
+    risk_free_rate_text = ('''The 1 year treasury yield at the start is {:,.02}%.  Start date is {} and end date is {}.
 '''.format(rfr, start_date, end_date))
-   
-    # separator = '<br>'
-    # combine_string = separator.join(port_stats_text)
+    
+    # separator = '<br>'
+    # combine_string = separator.join(port_stats_text)
  
-    port_stats_text = dcc.Markdown('''
-    ## Best Portfolio Metrics Based on {:,} Monte Carlo Simulation Runs:
-                                   
-    - Portfolio Expected Annual Return = {:.02f}%
-    - Portfolio Standard Deviation (Volatility) = {:.02f}%
-    - Sharpe Ratio = {:.02f}
-    - Final Value = ${:,.2f}
-    - Return on Investment = {:.02f}%''' .format(sim_runs, optimal_portfolio_return * 100,optimal_volatility*100, optimal_sharpe_ratio, highest_final_value, optimal_return_on_investment))
+    port_stats_text = dcc.Markdown('''
+    ## Best Portfolio Metrics Based on {:,} Monte Carlo Simulation Runs:
+                                    
+    - Portfolio Expected Annual Return = {:.02f}%
+    - Portfolio Standard Deviation (Volatility) = {:.02f}%
+    - Sharpe Ratio = {:.02f}
+    - Final Value = ${:,.2f}
+    - Return on Investment = {:.02f}%''' .format(sim_runs, optimal_portfolio_return * 100,optimal_volatility*100, optimal_sharpe_ratio, highest_final_value, optimal_return_on_investment))
  
-    best_weights_df = optimal_portfolio_df.head(1)
-    best_weights_df = pd.concat([best_weights_df,optimal_portfolio_df.tail(1)], ignore_index=True)
-    ccolumn = []
+    best_weights_df = optimal_portfolio_df.head(1)
+    best_weights_df = pd.concat([best_weights_df,optimal_portfolio_df.tail(1)], ignore_index=True)
+    ccolumn = []
  
-    for col in best_weights_df.columns:
-        types = best_weights_df[col].dtype
+    for col in best_weights_df.columns:
+        types = best_weights_df[col].dtype
  
-        if col == 'Portfolio Daily Return [%]':
-            item = dict(id=col, name = col, type = 'numeric', format=Format(precision=2, scheme=Scheme.percentage))
+        if col == 'Portfolio Daily Return [%]':
+            item = dict(id=col, name = col, type = 'numeric', format=Format(precision=2, scheme=Scheme.percentage))
  
-            # ccolumn.append(item)
+            # ccolumn.append(item)
  
-        elif types == "float64":
-            item = dict(id=col, name = col, type = 'numeric', format=Format(precision=2, symbol=Symbol.yes, scheme=Scheme.fixed).group(True))
+        elif types == "float64":
+            item = dict(id=col, name = col, type = 'numeric', format=Format(precision=2, symbol=Symbol.yes, scheme=Scheme.fixed).group(True))
  
-            ccolumn.append(item)
-        else:
-            item = dict(id=col, name = col, type='datetime')
-            ccolumn.append(item)
+            ccolumn.append(item)
+        else:
+            item = dict(id=col, name = col, type='datetime') 
+            ccolumn.append(item)
  
-   
+    
  
  
-    return fig, fig2, port_stats_text, risk_free_rate_text, dash_table.DataTable(
-                                                                                    data=best_weights_df.to_dict('records'),
-                                                                                    columns=ccolumn,
-                                                                                    style_cell={'textAlign':'left'}
-                                                                                )
- 
+    return fig, fig2, port_stats_text, risk_free_rate_text, dash_table.DataTable(
+                                                                                    data=best_weights_df.to_dict('records'),
+                                                                                    columns=ccolumn,
+                                                                                    style_cell={'textAlign':'left'}
+                                                                                )
  
  
 if __name__ =='__main__':
-  app.run_server(debug=True)
+  app.run(debug=True, jupyter_mode="external")
+
